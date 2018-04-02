7c71c6f9e81bb9ef3da73472c8a28f61aa9fc4b1
NickChlam <Nick.Chlam@rht.com>
2018-04-01 20:14:03 -0600

    set up structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
 import { RecipiesComponent } from './recipies/recipies.component';
-
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipiesDetailComponent } from './recipies/recipies-detail/recipies-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipiesItemComponent} from './recipies/recipies-list/recipies-item/recipies-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipiesDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipiesItemComponent
   ],
   imports: [
     BrowserModule
